ae4e0ee38e480b6011529b4d4dac41b649564009
Christian Torrico <christian.torrico-contractor@procore.com>
2024-08-26 22:38:37 -0500

    added Main class

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,29 @@
 package com.locotoinnovations
 
-fun main() {
-    println("Hello World!")
+import com.locotoinnovations.app.DaggerAppComponent
+import com.locotoinnovations.repository.PostRepository
+import kotlinx.coroutines.runBlocking
+import javax.inject.Inject
+
+class Main {
+
+    @Inject
+    lateinit var postRepository: PostRepository
+
+    fun run() = runBlocking {
+        try {
+            val todos = postRepository.getPosts()
+            todos.forEach{ println(it) }
+        } catch (e: Exception) {
+            println(e.message)
+        }
+    }
+}
+
+
+fun main() = runBlocking {
+    val appComponent = DaggerAppComponent.create()
+    val main = Main()
+    appComponent.inject(main)
+    main.run()
 }
