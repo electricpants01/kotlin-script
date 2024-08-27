4f167a1dc806c4a0cf899004b40f7f4dbd44fc7d
Christian Torrico <christian.torrico-contractor@procore.com>
2024-08-26 22:38:06 -0500

    added dagger and retrofit dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "1.9.23"
+    kotlin("kapt") version "1.9.10"
 }
 
 group = "com.locotoinnovations"
@@ -11,6 +12,12 @@ repositories {
 
 dependencies {
     testImplementation("org.jetbrains.kotlin:kotlin-test")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.3")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+    implementation("com.google.dagger:dagger:2.47")
+    kapt("com.google.dagger:dagger-compiler:2.47")
 }
 
 tasks.test {
